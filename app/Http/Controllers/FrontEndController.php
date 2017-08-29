12e5c0c15add8a0b1d3a2bf7d19fe10ff999fba4
noorhaziq <code.noorhaziq@gmail.com>
2017-08-30 01:15:11 +0800

    Dynamic content display 2

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -19,7 +19,9 @@ class FrontEndController extends Controller
         return view('index')
             ->with('title', Setting::first()->site_name)
             ->with('categories', Category::take(4)->get())
-            ->with('first_post', Post::orderBy('created_at', 'asc')->first());
+            ->with('first_post', Post::orderBy('created_at', 'desc')->first())
+            ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
+            ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first());
     }
 
     /**
